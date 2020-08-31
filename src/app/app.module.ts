674188bac5388b2cf2d99ca2d9861700c2cd0b0c
Mahelet.K <maheletkassa1@gmail.com>
2020-08-31 11:22:11 -0400

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DemoComponent } from './demo/demo.component';
+
+import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider,
+  AmazonLoginProvider,
+} from 'angularx-social-login';
+
+@NgModule({
+  declarations: [AppComponent, NavbarComponent, DemoComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule, SocialLoginModule],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: true,
+        providers: [
+         
+          {
+            id: FacebookLoginProvider.PROVIDER_ID,
+            provider: new FacebookLoginProvider('731585047573887'),
+          }
+        ],
+      } as SocialAuthServiceConfig,
+    }
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
